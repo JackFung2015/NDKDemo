43ddaea3df4b08dd19370f45a2654e663c163a5b
JackFung2015 <374826704fzy@sina.com>
2018-07-16 10:05:47 +0800

    Test Commit

--- CMakeNdkDemo/src/main/java/com/example/cmakendkdemo/MainActivity.java
+++ CMakeNdkDemo/src/main/java/com/example/cmakendkdemo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        //test commit
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
